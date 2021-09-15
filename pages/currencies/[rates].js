1e1553730ef4c59ebd37c6669efa74f3144e8b46
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-14 23:32:21 -0300

    itinital commit

---
+++ pages/currencies/[rates].js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const CurrencyRates = () => {
+    const router = useRouter()
+
+
+    return (
+        <div>
+            Esta es la pagina de los rates
+            {router.query.rates}
+        </div>
+    )
+}
+
+export default CurrencyRates

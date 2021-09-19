e9481ba0bf25436e128b7b6a03f05620488f24ca
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-19 15:37:19 -0300

    chenged to tsx

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <menu>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </menu>
+    </div>
+  )
+}
---
+++ pages/currencies/[rates].tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const CurrencyRates = () => {
+  const router = useRouter()
+  return (
+    <div>
+      Esta es la pagina de los rates
+      {router.query.rates}
+    </div>
+  )
+}
+
+export default CurrencyRates

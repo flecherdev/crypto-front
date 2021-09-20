17b1fec04962ccd8921fd6a8d1e04031837d0293
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-09-19 23:18:38 -0300

    added semantic-ui

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Navbar } from '../Navbar/Navbar'
+import { Navbar } from '@components/Navbar/Navbar'
+import styles from './layout.module.css'
 
 export const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <Navbar />
       {children}
       <footer> this is the footer </footer>
